30d32c099227bc69409dbb039aed9ffffb3a6399
Gabriel <konk3r@gmail.com>
2022-08-01 00:23:15 +0100

    Add log files

--- python-test.py
+++ python-test.py
@@ -2,7 +2,9 @@ import json
 from time import time
 from os.path import exists as file_exists
 
-RECORD_FILE_PATH = "/var/casadetasha/water-alert/last-collection-log.txt"
+ROOT_PATH = "/var/casadetasha/water-alert/"
+EVENT_LOGS_FILE_PATH = ROOT_PATH + "collection_event_logs.txt"
+LAST_EVENT_FILE_PATH = ROOT_PATH + "last_collection_event.txt"
 TEN_MINUTES_TO_SECONDS = 60*10
 
 BLANK_EVENT_JSON = json.dumps({
@@ -10,8 +12,8 @@ BLANK_EVENT_JSON = json.dumps({
         "eventType": "awaiting_first_event"
 })
 
-if file_exists(RECORD_FILE_PATH):
-    textFile = open(RECORD_FILE_PATH, "r")
+if file_exists(LAST_EVENT_FILE_PATH):
+    textFile = open(LAST_EVENT_FILE_PATH, "r")
     fileContent = textFile.read()
     textFile.close()
 else:
@@ -25,7 +27,11 @@ print("Last loaded time: " + loadedTimeString)
 
 workingJson["eventTimeInSeconds"] = int(time())
 
-textFile = open(RECORD_FILE_PATH, "w")
+textFile = open(LAST_EVENT_FILE_PATH, "w")
 textFile.write(json.dumps(workingJson) + "\n")
+textFile.close()
+
+textFile = open(EVENT_LOGS_FILE_PATH, "a")
+textFile.write(json.dumps(workingJson) + "\n\n------------\n\n")
 textFile.close()
 
